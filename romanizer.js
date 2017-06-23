c1d89149944a5ce8065ba5cf89a15245321b7638
oliverjam <oliverjamesphillips@gmail.com>
2017-06-23 12:28:33 +0100

    Refactor to generalise 6 -> VI solution

--- romanizer.js
+++ romanizer.js
@@ -31,17 +31,40 @@
 // }
 
 // This continues using the same mechanism as before - appending to a result variable
+// Again there is unnecessary duplication here
+// Time to refactor
+// function romanizer(num) {
+//   var result = '';
+//   if (num === 6) {
+//     result += 'VI';
+//   } else if (num === 5) {
+//     result += 'V';
+//   } else if (num === 4) {
+//     result += 'IV';
+//   } else {
+//     while (num > 0) {
+//       result += 'I';
+//       num -= 1;
+//     }
+//   }
+//   return result;
+// }
+
+// It looks like from 5 up we need to start with V, then append Is
+// We already wrote code to append Is, so let's leverage that
 function romanizer(num) {
+  var remaining = num;
   var result = '';
-  if (num === 5) {
+  if (remaining >= 5) {
     result += 'V';
+    remaining -= 5;
   } else if (num === 4) {
     result += 'IV';
-  } else {
-    while (num > 0) {
-      result += 'I';
-      num -= 1;
-    }
+    remaining -= 4;
+  }
+  while (remaining > 0) {
+    result += 'I';
+    remaining -= 1;
   }
   return result;
 }

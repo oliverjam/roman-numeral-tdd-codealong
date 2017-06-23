fd588494cfc081ef30ee4e8d0743f8d6a6d4fe6e
oliverjam <oliverjamesphillips@gmail.com>
2017-06-23 11:36:30 +0100

    Export romanizer and test it returns a string

---
+++ romanizer.js
@@ -0,0 +1,7 @@
+function romanizer(num) {
+  return num.toString();
+}
+
+if (module.exports !== undefined) {
+  module.exports = romanizer;
+}
--- romanizer.test.js
+++ romanizer.test.js
@@ -1,6 +1,14 @@
 var test = require('tape');
+var romanizer = require('./romanizer');
 
 test('Tape is up and running', function(t) {
   t.equal(1, 1, 'One should equal one');
+  t.end();
+});
+
+test('Romanizer returns a string', function(t) {
+  var actual = romanizer(2);
+  var expected = '2';
+  t.equal(actual, expected, 'Two should equal two');
   t.end();
 });
